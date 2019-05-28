0abb57a45ddef29e2df17f80304af293a7a1af3a
Fabrício Santos Barbacena <fabricio.barbacena@capes.gov.br>
2019-05-28 16:58:10 -0300

    Makegrid() func first attemp (grid disappearing)

--- designs.js
+++ designs.js
@@ -3,8 +3,45 @@
 
 // When size is submitted by the user, call makeGrid()
 
+
+
 function makeGrid() {
-
-// Your code goes here!
-
+    var height = Number(document.getElementById('inputHeight').value)
+    var width = Number(document.getElementById('inputWidth').value)
+    for (var row = 0; row < height; row++) {
+        var newTr = document.createElement('tr')
+        idNameTr = 'tr'.concat(row)
+        newTr.setAttribute('id', idNameTr)
+        var canvas = document.getElementById('pixelCanvas')
+        canvas.appendChild(newTr)
+        for (var col = 0; col < width; col++) {
+            var newTd = document.createElement('td')
+            idNameTd = 'td'.concat(row, col)
+            newTd.setAttribute('id', idNameTd)
+            thisTr = document.getElementById(idNameTr)
+            thisTr.appendChild(newTd)
+        }
+    }
 }
+
+document.getElementById('submitButton').addEventListener('click', makeGrid, false);
+
+
+/* 
+
+function makeGrid() {
+    var height = Number(document.getElementById('inputHeight').value)
+    var width = Number(document.getElementById('inputWidth').value)
+    var canvas = document.getElementById('pixelCanvas')
+    for (var row = 0; row < height; row++) {
+        var newTr = canvas.insertRow(row)
+        idNameTr = 'row'.concat(row)
+        newTr.setAttribute('id', idNameTr)
+        for (var cel = 0; cel < width; cel++) {
+            var newTd = canvas.insertCell(cel)
+            idNameTd = 'cell'.concat(row, cel)
+            newTd.setAttribute('id', idNameTd)
+}
+
+*/
+
